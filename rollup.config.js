84f04f59537275e6c02c47524f0eca4f3a465a30
Ed Prince <edward_prince@hotmail.com>
2019-04-16 11:35:01 +0100

    final version running on live server wrapper

--- rollup.config.js
+++ rollup.config.js
@@ -18,9 +18,8 @@ export default {
     liveServer({
       port: 8001,
       host: "0.0.0.0",
-      root: "",
+      root: "./build",
       file: "index.html",
-      mount: [["./", "./"], ['/node_modules', './node_modules']],
       open: false,
       wait: 500
     }),
