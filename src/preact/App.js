3f626a27d70da3d7ad1f9c8341887bc0393bf8f6
danprince <dan@kumu.io>
2019-04-17 15:00:21 +0100

    converts app to preact

---
+++ src/preact/App.js
@@ -0,0 +1,21 @@
+import '../common/App.css';
+import { h, Component } from  'preact';
+import 'bootstrap/dist/css/bootstrap.css';
+import Board from "./Board";
+import Input from "./Input";
+import factorial from "../common/factorial";
+
+console.log(factorial(1000));
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Board />
+        <Input />
+      </div>
+    );
+  }
+}
+
+export default App;
