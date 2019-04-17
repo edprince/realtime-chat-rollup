35cb5bb760f89682b9c2c0024256fa772af347f4
danprince <dan@kumu.io>
2019-04-17 13:55:43 +0100

    separates rollup configs for development and production

---
+++ rollup.production.js
@@ -0,0 +1,51 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import css from 'rollup-plugin-css-porter';
+import { uglify } from 'rollup-plugin-uglify';
+
+// This config will produce optimized bundles that can be used for benchmarks.
+// However it won't reload during development.
+
+let framework = process.env.framework || "react";
+
+export default {
+  input: `src/${framework}/index.js`,
+  output: {
+    file: 'build/js/bundle.js',
+    format: 'iife',
+    sourceMap: 'inline'
+  },
+  plugins: [
+    resolve({ browser: true }),
+
+    babel({
+      presets: ['@babel/env', '@babel/preset-react'],
+      exclude: 'node_modules/**'
+    }),
+
+    replace({
+      'process.env.NODE_ENV': JSON.stringify("production")
+    }),
+
+    commonjs({
+      include: [
+        'node_modules/**',
+      ],
+      exclude: [
+        'node_modules/process-es6/**',
+      ],
+      namedExports: {
+        'node_modules/react/index.js': ['Children', 'Component', 'PropTypes', 'createElement'],
+        'node_modules/react-dom/index.js': ['render'],
+      }
+    }),
+
+    css({
+      minified: 'build/css/bundle.css'
+    }),
+
+    uglify(),
+  ]
+}
